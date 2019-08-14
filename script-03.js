c955ee27ea55bb67b1cf4ef6777e480c9735ef4e
kuomolew <kuomolew@gmail.com>
2019-08-14 17:13:40 +0300

    test

--- script-03.js
+++ script-03.js
@@ -1,10 +1,22 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+calculateAge(1965);
+// retirement(1990);
+
+function calculateAge(year) {
+    console.log(2016 - year);
+}
+
+var retirement = function(year) {
+    console.log(65-(2016 - year));
+}
 
 
-
-
+// variables
+console.log(age);
+var age = 23;
+console.log(age);
 
 
 
