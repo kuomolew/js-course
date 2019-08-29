13fea28dd6e62a07a5cca7a05bc3be1856cb0363
kuomolew <kuomolew@gmail.com>
2019-08-29 19:28:34 +0300

    challenge finish

--- 07-es6/challenge.js
+++ 07-es6/challenge.js
@@ -69,15 +69,16 @@ addNewElement(oakPark);
 const oceanAve = new Street('Ocean Avenue', 1999, 3.2, 'big');
 const evergreenStr = new Street('Evergreen Street', 2008, 1.2, 'small');
 const fourthStr = new Street('4th Street', 2015, 2.2);
-const sunsetBou = new Street('Sunset Boulevard', 1982, 4.6, 'huge');
+const sunsetBou = new Street('Sunset Boulevard', 1982, 4.6, 'tiny');
 
 addNewElement(oceanAve);
 addNewElement(evergreenStr);
 addNewElement(fourthStr);
 addNewElement(sunsetBou);
 
+
 // counting of general park report
-function parkReports() {
+function parksReport() {
     const names = town.parks.name;
     const years = town.parks.buildYear;
     const trees = town.parks.treesNumber;
@@ -111,17 +112,58 @@ function parkReports() {
         console.log(`${names[index]} has more than 1000 trees.`);
     }
 
+    console.log('----PARKS REPORT----');
     averageParkAge();
     treeDensity();
     moreThan1000();
 }
 
+// general streets report
+function streetsReport() {
+    const names = town.streets.name;
+    const years = town.streets.buildYear;
+    const length = town.streets.length;
+    const size = town.streets.size;
+
+    number = names.length;
+
+    // counting of total and average length of all streets 
+    function totalAverageLength() {
+        let total, average;
+        total = 0;
+        for (cur of length) {
+            total += cur;
+        }
+        average = total / number;
+        console.log(`Our ${number} streets have a total length of ${total} km, with an average of ${average} km.`);
+    }
+
+    // size classification
+    function sizeClassification(){
+        for (let i = 0; i < number; i++) {
+            if (length[i] <= 0.5){
+                size[i] = 'tiny';
+            } else if (length[i] > 0.5 && length[i] <= 1) {
+                size[i] = 'small';
+            } else if (length[i] > 1 && length[i] <= 2) {
+                size[i] = 'normal';
+            } else if (length[i] > 2 && length[i] <=3) {
+                size[i] = 'big';
+            } else if (length[i] > 3) {
+                size[i] = 'huge';
+            } else {
+                size[i] = 'normal';
+            }
+
+            console.log(`${names[i]}, built in ${years[i]}, is a ${size[i]} street.`);
+        }
+    }
+
+    console.log('----STREETS REPORT----');
+    totalAverageLength();
+    sizeClassification();
+}
 
 
-
-
-
-
-
-
-parkReports();
+parksReport();
+streetsReport();
