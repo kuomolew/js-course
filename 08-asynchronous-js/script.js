46f9e2623bf3e0b0cb8195b2f17c96c2c8ff5218
kuomolew <kuomolew@gmail.com>
2019-09-01 17:55:45 +0300

    122

--- 08-asynchronous-js/script.js
+++ 08-asynchronous-js/script.js
@@ -1,1 +1,14 @@
-console.log('Test');
+const second = () => {
+    console.log('Second');
+    setTimeout(() => {
+        console.log('Here\'s Johnny!')
+    }, 2000)
+}
+
+const first = () => {
+    console.log('hi Test');
+    second();
+    console.log('The end');
+}
+
+first();
